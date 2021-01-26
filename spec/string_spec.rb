2b2680ec401893942bb6e6ca02a258f0d318d014
Mary Jo <mjg13@rice.edu>
2021-01-26 12:52:55 -0600

    Create string_spec.rb (#86)
    * Create string_spec.rb
    
    * change test case to "CHEESE!"

---
+++ spec/string_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe String do
+
+  describe '#uncapitalize' do
+    it 'downcases the first letter of self, returning a new string' do
+      expect('CHEESE!'.uncapitalize).to eq('cHEESE!')
+    end
+  end
+
+end
