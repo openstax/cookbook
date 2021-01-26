7ae20437de49df13843e7efb73953ca345f684f2
Alan Cooke <12678235+Stackblocks@users.noreply.github.com>
2021-01-26 17:10:18 -0600

    Create an initial spec for Kitchen::Pantry (#87)
    * Create a spec for Kitchen::Pantry
    
    - Create a spec file and rudimentary tests
    
    Re: #48
    
    * Get all but two of the tests working
    
    - The `.instance` test is returning a positive
    - The `.store` test is returning a positive
    - The `.get` test is returning a positive
    - The `.get!` raise error test is still failing for mysterious reasons
    - The `.each` test is still failing because I don't understand it.
    - The `.size` test is returning a positive
    
    Re: #48
    
    * Gets `.get!` test up and running
    
    - Needed curly brace for the `expect{...}.to` block instead of paren.
    
    Re: #48
    
    * Group tests requiring dummy data into a context
    
    In order to DRY out code and not repeat the "add one item to pantry"
    line four times I placed them into a context block with a `before`
    so the line only appears once.
    
    Re: #48
    
    * Get the `.each` test to pass
    
    Built out based on the comment on the [clipboard spec
    pr](https://github.com/openstax/kitchen/pull/78#discussion_r564108541)
    
    Re: #48
    
    * Fixed a RuboCop error on a sentence-case context line
    
    * Clears up describtive lines and beefs up tests
    
    - Added the tests JP mentioned in his review
    - Formatted the file to output clearer results
    - Resolve conflicts
    
    Re: #48

---
+++ spec/pantry_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+RSpec.describe Kitchen::Pantry do
+  let(:instance) { described_class.new }
+
+  describe 'initialize' do
+    it 'creates a pantry object' do
+      expect(instance).to be_truthy
+    end
+  end
+
+  context 'when at least one item is in the pantry' do
+    before { instance.store 'bar', label: 'foo' }
+
+    describe '#get' do
+      it 'returns a present item with either a string or symbol label' do
+        expect(instance.get(:foo)).to eq('bar')
+        expect(instance.get('foo')).to eq('bar')
+      end
+
+      it 'returns nil when an item is not present' do
+        expect(instance.get(:blah)).to be_nil
+      end
+    end
+
+    describe '#get!' do
+      it 'returns a present item with either a string or symbol label' do
+        expect(instance.get!(:foo)).to eq('bar')
+        expect(instance.get!('foo')).to eq('bar')
+      end
+
+      it 'raises if requested item is not in pantry' do
+        expect { instance.get!('thud') }.to raise_error(
+          Kitchen::RecipeError, "There is no pantry item labeled 'thud'"
+        )
+      end
+    end
+
+    describe '#size' do
+      it 'returns the number of items in the pantry' do
+        expect(instance.size).to eq(1)
+      end
+    end
+
+    describe '#each' do
+      it 'iterates over the pantry items' do
+        instance.store 'baz', label: 'qux'
+        expect { |block| instance.each(&block) }.to yield_successive_args([:foo, 'bar'], [:qux, 'baz'])
+      end
+    end
+  end
+end
