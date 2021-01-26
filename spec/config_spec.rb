12a76f67ac52c4101ecbd1c4adbc7857d2f6bba6
Mary Jo <mjg13@rice.edu>
2021-01-26 12:52:04 -0600

    Creates config_spec.rb (#85)
    * Create config_spec.rb
    
    * fix rubycop errors

---
+++ spec/config_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe Kitchen::Config do
+  let(:foobar) { described_class.new(hash: {}, selectors: 'foo') }
+
+  describe '.initialize' do
+    it 'creates a new Config instance' do
+      expect(described_class.new).to be_truthy
+    end
+  end
+
+  describe '#selectors' do
+    it 'reads selectors' do
+      expect(foobar.selectors).to eq('foo')
+    end
+  end
+
+  describe 'self.new_from_file' do
+    it "isn't implemented yet" do
+      expect { described_class.new_from_file('bleh') }.to raise_error('NYI')
+    end
+  end
+
+end
