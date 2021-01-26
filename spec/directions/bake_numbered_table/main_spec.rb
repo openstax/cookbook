bdc65250c31a1dbbf91f0ad3072ea7a3bb1efe6f
Kendra Swanson <kcs7@rice.edu>
2021-01-26 14:00:09 -0500

    Add specs for main footnotes, index, and numbered table (#90)

---
+++ spec/directions/bake_numbered_table/main_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe Kitchen::Directions::BakeNumberedTable do
+  it 'calls v1' do
+    expect_any_instance_of(Kitchen::Directions::BakeNumberedTable::V1).to receive(:bake).with(table: 'blah', number: '7')
+    described_class.v1(table: 'blah', number: '7')
+  end
+end
