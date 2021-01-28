7c8da8e5e6043b67d32d3c8935e8ccf8aef8c744
Alan Cooke <12678235+Stackblocks@users.noreply.github.com>
2021-01-28 16:50:46 -0600

    Create a spec for MoveTitleTextToSpan (#97)
    * Create a spec for MoveTitleTextToSpan
    
    Re: #70
    
    * Remove extra step & clear up test message

---
+++ spec/directions/move_title_text_into_span_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe Kitchen::Directions::MoveTitleTextIntoSpan do
+
+  let(:title) do
+    new_element(
+      <<~HTML
+        <h1 data-type="document-title">foo</h1>
+      HTML
+    )
+  end
+
+  it 'works' do
+    described_class.v1(title: title)
+    expect(title).to match_normalized_html(
+      <<~HTML
+        <h1 data-type="document-title">
+          <span data-type="" itemprop="" class="os-text">foo</span>
+        </h1>
+      HTML
+    )
+  end
+end
