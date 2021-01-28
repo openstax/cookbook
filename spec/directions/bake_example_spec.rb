bf4670fa17e59286cf40795c5bebe918a575ee35
Kendra Swanson <kcs7@rice.edu>
2021-01-28 17:55:59 -0500

    Specs for BakeExample (#98)

---
+++ spec/directions/bake_example_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+RSpec.describe Kitchen::Directions::BakeExample do
+  let(:example) do
+    book_containing(html:
+      <<~HTML
+        <div data-type="chapter">
+          <div data-type="page">
+            <div data-type="document-title" id="auto_m68761_72010">Page 1 Title</div>
+            <div data-type='example' id='example-test'>
+              <span data-type="title">example title becomes h4</span>
+              <p>content</p>
+            </div>
+          </div>
+        </div>
+      HTML
+    ).chapters.pages.examples.first
+  end
+
+  it 'works' do
+    described_class.v1(example: example, number: 4, title_tag: 'title-tag-name')
+    expect(example).to match_normalized_html(
+      <<~HTML
+        <div data-type="example" id="example-test">
+          <title-tag-name class="os-title">
+            <span class="os-title-label">Example </span>
+            <span class="os-number">4</span>
+            <span class="os-divider"> </span>
+          </title-tag-name>
+          <div class="body">
+            <h4 data-type="title">example title becomes h4</h4>
+            <p>content</p>
+          </div>
+        </div>
+      HTML
+    )
+  end
+
+  it 'stores info in the pantry' do
+    expect { described_class.v1(example: example, number: 4, title_tag: 'title-tag-name') }.to change {
+      example.document.pantry(name: :link_text).get(example.id)
+    }.from(nil)
+  end
+end
