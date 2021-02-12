1f43b3f6d2ad039187c85add7cbb66595b4a6992
Kendra Swanson <kcs7@rice.edu>
2021-02-12 16:44:00 -0600

    Add BakeLinkPlaceholders class to base kitchen (#134)
    * Add BakeLinkPlaceholders class to base kitchen
    
    * Refactor to remove redundant argument

---
+++ lib/kitchen/directions/bake_link_placeholders.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Kitchen
+  module Directions
+    # Bake directions for link placeholders
+    #
+    module BakeLinkPlaceholders
+      def self.v1(book:)
+        book.search('a').each do |anchor|
+          next unless anchor.text == '[link]'
+
+          id = anchor[:href][1..-1]
+          replacement = book.document.pantry(name: :link_text).get(id)
+          if replacement.present?
+            anchor.replace_children(with: replacement)
+          else
+            # TODO: log a warning!
+            puts "warning! could not find a replacement for '[link]' on an element with ID '#{id}'"
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/directions/bake_link_placeholders_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Kitchen::Directions::BakeLinkPlaceholders do
+  let(:book) do
+    book_containing(html:
+      <<~HTML
+        <a>skip this link</a>
+        <a href='?key'>[link]</a>
+      HTML
+    )
+  end
+
+  let(:invalid_book) do
+    book_containing(html:
+      <<~HTML
+        <a>skip this link</a>
+        <a href='?invalid_key'>[link]</a>
+      HTML
+    )
+  end
+
+  before do
+    book.document.pantry(name: :link_text).store('Example x.y', label: 'key')
+  end
+
+  it 'bakes' do
+    described_class.v1(book: book)
+    expect(book.body).to match_normalized_html(
+      <<~HTML
+        <body>
+          <a>skip this link</a>
+          <a href='?key'>Example x.y</a>
+        </body>
+      HTML
+    )
+  end
+
+  it 'logs a warning' do
+    allow($stdout).to receive(:puts)
+    expect($stdout).to receive(:puts).with("warning! could not find a replacement for '[link]' on an element with ID 'invalid_key'")
+    described_class.v1(book: invalid_book)
+  end
+end
