26a142c604ec73a97d1f58e2ea4133b2d5fe9fd9
Kendra Swanson <kendraswan@gmail.com>
2021-02-24 21:09:31 +0000

    Outline of how to shorten chemistry

---
+++ specs/chemistry/spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# A spec for a recipe will:
+#  1) run the kitchen bake script for that book on the shortened assembled file
+#  2) compare the normalized easybake file with the normalized output from kitchen
