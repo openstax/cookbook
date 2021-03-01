dc0ec33662d2583f1467a54a04f39e63aa667f97
KarinaMendez2 <13000365@galileo.edu>
2021-03-01 20:33:23 +0000

    requested changes

--- spec/helpers/match_helper.rb
+++ spec/helpers/match_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'digest'
+require 'tempfile'
 
 RSpec::Matchers.define(:be_same_file_as) do |expected_file_path|
   match do |actual_file_path|
@@ -9,5 +10,13 @@ RSpec::Matchers.define(:be_same_file_as) do |expected_file_path|
 
   def md5_hash(file_path)
     Digest::MD5.hexdigest(File.read(file_path))
+  end
+end
+
+RSpec::Matchers.define :bake_correctly do
+  match do |book|
+    actual_file = Tempfile.new(book)
+    `./bake -b #{book} -i spec/books/#{book}/input.xhtml -o #{actual_file.path}`
+    expect("spec/books/#{book}/expected_output.xhtml").to be_same_file_as(actual_file.path)
   end
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,23 +2,8 @@
 
 require 'spec_helper'
 
-books = ['dummy'] # add finished books to this array
-
 RSpec.describe 'books' do
-  before(:all) do
-    books.each do |book|
-      # call the bake script for each book
-      output = `USE_LOCAL_KITCHEN=1 ./bake -b #{book} -i spec/books/#{book}/input.xhtml \
-        -o spec/books/#{book}/actual_output.xhtml`
-      puts output
-    end
-  end
-
-  books.each do |book|
-    it "#{book} matches expected output" do
-      expect("spec/books/#{book}/expected_output.xhtml").to be_same_file_as(
-        "spec/books/#{book}/actual_output.xhtml"
-      )
-    end
+  it 'bakes dummy' do
+    expect('dummy').to bake_correctly
   end
 end
