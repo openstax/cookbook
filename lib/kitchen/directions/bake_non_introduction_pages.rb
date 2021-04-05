c3dd4090232aff599a976c30b7aee201255c2424
Karina Méndez <13000365@galileo.edu>
2021-04-05 10:37:57 -0600

    Bake Non Intro Pages (#168)
    * changing name to plural and moving loop into bake file
    
    * indentation

---
+++ lib/kitchen/directions/bake_non_introduction_pages.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Kitchen
+  module Directions
+    module BakeNonIntroductionPages
+      def self.v1(chapter:)
+        chapter.non_introduction_pages.each do |page|
+          number = "#{chapter.count_in(:book)}.#{page.count_in(:chapter)}"
+
+          page.search("div[data-type='description']").each(&:trash)
+          page.add_class('chapter-content-module')
+
+          title = page.title
+          title.name = 'h2'
+          title.replace_children(with:
+            <<~HTML
+              <span class="os-number">#{number}</span>
+              <span class="os-divider"> </span>
+              <span data-type="" itemprop="" class="os-text">#{title.text}</span>
+            HTML
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/directions/bake_non_introduction_pages_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Kitchen::Directions::BakeNonIntroductionPages do
+  let(:chapter) do
+    book_containing(html:
+      one_chapter_with_one_page_containing(
+        <<~HTML
+          <div data-type="metadata" style="display: none;">
+            <div class="description" itemprop="description" data-type="description">
+              <ul>
+                <li>Use functional notation to evaluate a function.</li>
+              </ul>
+            </div>
+          </div>
+          <div data-type="document-title" id="anId">Review of Functions</div>
+        HTML
+      )
+    ).chapters.first
+  end
+
+  it 'works' do
+    described_class.v1(chapter: chapter)
+    expect(chapter).to match_normalized_html(
+      <<~HTML
+        <div data-type="chapter">
+          <div data-type="page" class="chapter-content-module">
+            <div data-type="metadata" style="display: none;">\n  \n</div>
+            <h2 data-type="document-title" id="anId"><span class="os-number">1.1</span>
+              <span class="os-divider"> </span>
+              <span data-type="" itemprop="" class="os-text">Review of Functions</span>
+            </h2>
+          </div>
+        </div>
+      HTML
+    )
+  end
+end
