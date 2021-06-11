b6f84fbe0382e5804f1447189ecbdfd3be3179e6
Kendra Swanson <kcs7@rice.edu>
2021-06-11 16:04:23 -0400

    fix table v2 (#270)

--- lib/kitchen/directions/bake_numbered_table/main.rb
+++ lib/kitchen/directions/bake_numbered_table/main.rb
@@ -6,6 +6,10 @@ module Kitchen
       def self.v1(table:, number:, always_caption: false)
         V1.new.bake(table: table, number: number, always_caption: always_caption)
       end
+
+      def self.v2(table:, number:)
+        V2.new.bake(table: table, number: number)
+      end
     end
   end
 end
--- spec/directions/bake_numbered_table/main_spec.rb
+++ spec/directions/bake_numbered_table/main_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Kitchen::Directions::BakeNumberedTable do
     expect_any_instance_of(Kitchen::Directions::BakeNumberedTable::V1).to receive(:bake).with(table: 'blah', number: '7', always_caption: false)
     described_class.v1(table: 'blah', number: '7', always_caption: false)
   end
+
+  it 'calls v2' do
+    expect_any_instance_of(Kitchen::Directions::BakeNumberedTable::V2).to receive(:bake).with(table: 'blah', number: '7')
+    described_class.v2(table: 'blah', number: '7')
+  end
 end
