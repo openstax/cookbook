fb89df86f17f30931c3e01cf348d4ba373ce68bb
ajluk <ajlukasik@gmail.com>
2021-06-16 16:43:54 +0000

    clean bake file sociology

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -14,4 +14,8 @@ RSpec.describe 'books' do
   it 'bakes u-physics' do
     expect('u-physics').to bake_correctly
   end
+
+  it 'bakes sociology' do
+    expect('sociology').to bake_correctly
+  end
 end
