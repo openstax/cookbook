0e62b0296cdc39143544820c217cd1e432099a83
KarinaMendez2 <13000365@galileo.edu>
2021-11-30 15:44:42 +0000

    update commit ref

--- kitchen_gem_versions.rb
+++ kitchen_gem_versions.rb
@@ -21,7 +21,7 @@ KITCHEN_VERSIONS = {
   dummy: '2.0.0',
   english_composition: {
     github: 'openstax/kitchen',
-    ref: 'cafc9b8d7eabcea9d0a2ba1461d0a543130d3aef'
+    ref: '6f483788e9f4f5f011dc552542c464b11ad12764'
   },
   entrepreneurship: { github: 'openstax/kitchen', ref: 'aea1ed51c4c9bcc797128d55ecbb74e26b542a11' },
   finance: '11.2.0',
