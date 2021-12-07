6e246bf253dacac0007e56f05cfe65b50760633c
wilkus27 <natalia.wilkus@openstax.pl>
2021-12-07 09:55:40 +0000

    write specs for business law and intellectual property

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -162,4 +162,12 @@ RSpec.describe 'books' do
   it 'bakes business ethics' do
     expect('business-ethics').to bake_correctly
   end
+
+  it 'bakes business law' do
+    expect('business-law').to bake_correctly_with('business-ethics')
+  end
+
+  it 'bakes intellectual property' do
+    expect('intellectual-property').to bake_correctly_with('business-ethics')
+  end
 end
