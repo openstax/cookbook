640a26d3af244df5cded11e96c1fd1fd2fdfd536
KarinaMendez2 <13000365@galileo.edu>
2022-02-16 15:55:41 -0600

    bake injected exercises in your turn notes and check your understanding section

--- lib/recipes/contemporary-math/strategy.rb
+++ lib/recipes/contemporary-math/strategy.rb
@@ -3,16 +3,6 @@
 # Contemporary answer key strategy
 class Strategy
   def bake(chapter:, append_to:)
-    # Hacky numbering fix
-    chapter.notes('$.your-turn').exercises.each do |exercise|
-      solution = exercise.solution
-      next unless solution
-
-      number = exercise.ancestor(:note).count_in(:chapter)
-      solution.first('a.os-number').inner_html = number.to_s
-      solution.first('span.os-divider').inner_html = '. '
-    end
-
     Kitchen::Directions::MoveSolutionsFromNumberedNote.v1(
       chapter: chapter, append_to: append_to, note_class: 'your-turn'
     )
@@ -27,7 +17,7 @@ class Strategy
     end
 
     # Bake other exercise sections
-    classes = %w[chapter-review chapter-test]
+    classes = %w[chapter-review chapter-test check-understanding]
     classes.each do |klass|
       Kitchen::Directions::MoveSolutionsFromExerciseSection.v1(
         chapter: chapter, append_to: append_to, section_class: klass
