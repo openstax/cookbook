98b39e8c4f51745cad5a1863d1cdeafdcf262056
ajluk <ajlukasik@gmail.com>
2022-05-16 16:37:59 +0000

    add support for baking injected exercises /solutions in appendices

--- lib/kitchen/directions/move_solutions_to_answer_key/move_solutions_from_exercise_section.rb
+++ lib/kitchen/directions/move_solutions_to_answer_key/move_solutions_from_exercise_section.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Kitchen::Directions::MoveSolutionsFromExerciseSection
-  def self.v1(chapter:, append_to:, section_class:, title_number: nil)
+  def self.v1(chapter:, append_to:, section_class:, title_number: nil, in_appendix: false)
     V1.new.bake(chapter: chapter, append_to: append_to, section_class: section_class,
-                title_number: title_number)
+                title_number: title_number, in_appendix: in_appendix)
   end
 
   class V1
-    def bake(chapter:, append_to:, section_class:, title_number:)
+    def bake(chapter:, append_to:, section_class:, title_number:, in_appendix:)
       solutions_clipboard = chapter.search("section.#{section_class}").solutions.cut
 
       return if solutions_clipboard.items.empty?
@@ -15,6 +15,8 @@ module Kitchen::Directions::MoveSolutionsFromExerciseSection
       title_text = \
         if title_number
           I18n.t(:"eoc.#{section_class}", number: title_number)
+        elsif in_appendix
+          I18n.t(:"appendix_sections.#{section_class}")
         else
           I18n.t(:"eoc.#{section_class}")
         end
