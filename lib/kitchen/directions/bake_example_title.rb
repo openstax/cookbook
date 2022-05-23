bd50d115899f1ee12479373a4406660bb4a06c87
ajluk <ajlukasik@gmail.com>
2022-05-23 15:49:08 +0000

    create bake exmaple title direction

---
+++ lib/kitchen/directions/bake_example_title.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Kitchen
+  module Directions
+    module BakeExampleTitle
+      def self.v1(example:)
+        example.exercises.each do |exercise|
+          problem = exercise.problem
+          problem.prepend(child:
+            <<~HTML
+              <h4 data-type="problem-title">
+                <span class="os-title-label">#{I18n.t(:problem)}</span>
+              </h4>
+            HTML
+          )
+        end
+      end
+    end
+  end
+end
