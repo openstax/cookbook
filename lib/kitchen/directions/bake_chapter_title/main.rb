ebc7e486eabc5e68f70ee3227b8a45f7e35568a9
ajluk <ajlukasik@gmail.com>
2022-07-25 15:59:07 +0000

    fix chapter title changes

--- lib/kitchen/directions/bake_chapter_title/main.rb
+++ lib/kitchen/directions/bake_chapter_title/main.rb
@@ -3,8 +3,8 @@
 module Kitchen
   module Directions
     module BakeChapterTitle
-      def self.v1(book:)
-        V1.new.bake(book: book, cases: false)
+      def self.v1(book:, cases: false)
+        V1.new.bake(book: book, cases: cases)
       end
     end
   end
--- spec/kitchen_spec/directions/bake_chapter_title/v1_spec.rb
+++ spec/kitchen_spec/directions/bake_chapter_title/v1_spec.rb
@@ -29,4 +29,41 @@ RSpec.describe Kitchen::Directions::BakeChapterTitle::V1 do
 
     expect(book1.body.children.to_s).to match_snapshot_auto
   end
+
+  context 'when book uses grammatical cases' do
+    it 'uses nominative case in title' do
+      with_locale(:pl) do
+        stub_locales({
+          'chapter': {
+            'nominative': 'Rozdział',
+            'genitive': 'Rozdziału'
+          }
+        })
+        described_class.new.bake(book: book1, cases: true)
+
+        expect(book1.body.children.to_s).to match_normalized_html(
+          <<~HTML
+            <div data-type="chapter">
+              <h1>dummy</h1>
+              <h1 data-type="document-title" id="chapTitle1">
+                <span class="os-part-text">Rozdział </span>
+                <span class="os-number">1</span>
+                <span class="os-divider"> </span>
+                <span data-type="" itemprop="" class="os-text">chapter 1</span>
+              </h1>
+            </div>
+            <div data-type="chapter">
+              <h1>dummy</h1>
+              <h1 data-type="document-title" id="chapTitle2">
+                <span class="os-part-text">Rozdział </span>
+                <span class="os-number">2</span>
+                <span class="os-divider"> </span>
+                <span data-type="" itemprop="" class="os-text">chapter 2</span>
+              </h1>
+            </div>
+          HTML
+        )
+      end
+    end
+  end
 end
