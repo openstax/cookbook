e9fd048e1158fffb57a9c7e1f02c598c4660f074
ajluk <ajlukasik@gmail.com>
2022-07-25 16:19:51 +0000

    update answer key container chapter appendix titles cases

--- lib/kitchen/directions/move_solutions_to_answer_key/answer_key_inner_container.rb
+++ lib/kitchen/directions/move_solutions_to_answer_key/answer_key_inner_container.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 module Kitchen::Directions::AnswerKeyInnerContainer
-  def self.v1(chapter:, metadata_source:, append_to:, solutions_plural: true, in_appendix: false)
+  def self.v1(chapter:, metadata_source:, append_to:, solutions_plural: true, in_appendix: false, cases: false)
     V1.new.bake(
       chapter: chapter,
       metadata_source: metadata_source,
       append_to: append_to,
       solutions_plural: solutions_plural,
-      in_appendix: in_appendix
+      in_appendix: in_appendix,
+      cases: cases
     )
   end
 
   class V1
     renderable
 
-    def bake(chapter:, metadata_source:, append_to:, solutions_plural:, in_appendix:)
+    def bake(chapter:, metadata_source:, append_to:, solutions_plural:, in_appendix:, cases:)
       @solutions_or_solution = solutions_plural ? 'solutions' : 'solution'
       @uuid_key = if in_appendix
                     "appendix#{@solutions_or_solution}#{chapter.count_in(:book)}"
@@ -24,9 +25,9 @@ module Kitchen::Directions::AnswerKeyInnerContainer
       @metadata = metadata_source.children_to_keep.copy
       @composite_element = 'composite-page'
       @title = if in_appendix
-                 "#{I18n.t(:appendix)} #{[*('A'..'Z')][chapter.count_in(:book) - 1]}"
+                 "#{I18n.t("appendix#{'.nominative' if cases}")} #{[*('A'..'Z')][chapter.count_in(:book) - 1]}"
                else
-                 "#{I18n.t(:chapter)} #{chapter.count_in(:book)}"
+                 "#{I18n.t("chapter#{'.nominative' if cases}")} #{chapter.count_in(:book)}"
                end
       @main_title_tag = 'h2'
 
--- spec/kitchen_spec/directions/move_solutions_to_answer_key/answer_key_inner_container_spec.rb
+++ spec/kitchen_spec/directions/move_solutions_to_answer_key/answer_key_inner_container_spec.rb
@@ -57,4 +57,72 @@ RSpec.describe Kitchen::Directions::AnswerKeyInnerContainer do
       ).to match_snapshot_auto
     end
   end
+
+  context 'when book uses grammatical cases' do
+    it 'uses nominative case in title' do
+      with_locale(:pl) do
+        stub_locales({
+          'chapter': {
+            'nominative': 'Rozdział',
+            'genitive': 'Rozdziału'
+          }
+        })
+        expect(
+          described_class.v1(chapter: book.chapters.first, metadata_source: metadata_element, append_to: append_to, cases: true)
+        ).to match_normalized_html(
+          <<~HTML
+            <div class="os-eob os-solutions-container" data-type="composite-page" data-uuid-key=".solutions1">
+              <h2 data-type="document-title">
+                <span class="os-text">Rozdział 1</span>
+              </h2>
+              <div data-type="metadata" style="display: none;">
+                <h1 data-type="document-title" itemprop="name">Rozdział 1</h1>
+                <span data-type="revised" id="revised_copy_1">Revised</span>
+                <span data-type="slug" id="slug_copy_1">Slug</span>
+                <div class="authors" id="authors_copy_1">Authors</div>
+                <div class="publishers" id="publishers_copy_1">Publishers</div>
+                <div class="print-style" id="print-style_copy_1">Print Style</div>
+                <div class="permissions" id="permissions_copy_1">Permissions</div>
+                <div data-type="subject" id="subject_copy_1">Subject</div>
+              </div>
+            </div>
+          HTML
+        )
+      end
+    end
+
+    it 'uses nominative case in appendix title' do
+      with_locale(:pl) do
+        stub_locales({
+          'appendix': {
+            'nominative': 'Dodatek',
+            'genitive': 'Dodatku'
+          }
+        })
+        expect(
+          described_class.v1(chapter: book.pages.first, metadata_source: metadata_element, append_to: append_to, in_appendix: true, cases: true)
+        ).to match_normalized_html(
+          <<~HTML
+            <div class="os-eob os-solutions-container" data-type="composite-page" data-uuid-key=".appendixsolutions1">
+              <h2 data-type="document-title">
+                <span class="os-text">Dodatek A</span>
+              </h2>
+              <div data-type="metadata" style="display: none;">
+                <h1 data-type="document-title" itemprop="name">Dodatek A</h1>
+                <span data-type="revised" id="revised_copy_1">Revised</span>
+                <span data-type="slug" id="slug_copy_1">Slug</span>
+                <div class="authors" id="authors_copy_1">Authors</div>
+                <div class="publishers" id="publishers_copy_1">Publishers</div>
+                <div class="print-style" id="print-style_copy_1">Print Style</div>
+                <div class="permissions" id="permissions_copy_1">Permissions</div>
+                <div data-type="subject" id="subject_copy_1">Subject</div>
+              </div>
+            </div>
+          HTML
+        )
+      end
+    end
+
+  end
+
 end
