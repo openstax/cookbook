c9d274c6338e7e7394fa054ae8968b953d12dd11
ajluk <ajlukasik@gmail.com>
2022-07-28 16:22:35 +0000

    update recipes to use options in BakeAppendix, BakeNonIntroductionPages, refactor chapter_title, answerkey_inner_container latest specs to use snapshots

--- spec/kitchen_spec/directions/move_solutions_to_answer_key/answer_key_inner_container_spec.rb
+++ spec/kitchen_spec/directions/move_solutions_to_answer_key/answer_key_inner_container_spec.rb
@@ -76,25 +76,7 @@ RSpec.describe Kitchen::Directions::AnswerKeyInnerContainer do
         })
         expect(
           described_class.v1(chapter: book.chapters.first, metadata_source: metadata_element, append_to: append_to, options: { cases: true })
-        ).to match_normalized_html(
-          <<~HTML
-            <div class="os-eob os-solutions-container" data-type="composite-page" data-uuid-key=".solutions1">
-              <h2 data-type="document-title">
-                <span class="os-text">Rozdział 1</span>
-              </h2>
-              <div data-type="metadata" style="display: none;">
-                <h1 data-type="document-title" itemprop="name">Rozdział 1</h1>
-                <span data-type="revised" id="revised_copy_1">Revised</span>
-                <span data-type="slug" id="slug_copy_1">Slug</span>
-                <div class="authors" id="authors_copy_1">Authors</div>
-                <div class="publishers" id="publishers_copy_1">Publishers</div>
-                <div class="print-style" id="print-style_copy_1">Print Style</div>
-                <div class="permissions" id="permissions_copy_1">Permissions</div>
-                <div data-type="subject" id="subject_copy_1">Subject</div>
-              </div>
-            </div>
-          HTML
-        )
+        ).to match_snapshot_auto
       end
     end
 
@@ -108,25 +90,7 @@ RSpec.describe Kitchen::Directions::AnswerKeyInnerContainer do
         })
         expect(
           described_class.v1(chapter: book.pages.first, metadata_source: metadata_element, append_to: append_to, options: { in_appendix: true, cases: true })
-        ).to match_normalized_html(
-          <<~HTML
-            <div class="os-eob os-solutions-container" data-type="composite-page" data-uuid-key=".appendixsolutions1">
-              <h2 data-type="document-title">
-                <span class="os-text">Dodatek A</span>
-              </h2>
-              <div data-type="metadata" style="display: none;">
-                <h1 data-type="document-title" itemprop="name">Dodatek A</h1>
-                <span data-type="revised" id="revised_copy_1">Revised</span>
-                <span data-type="slug" id="slug_copy_1">Slug</span>
-                <div class="authors" id="authors_copy_1">Authors</div>
-                <div class="publishers" id="publishers_copy_1">Publishers</div>
-                <div class="print-style" id="print-style_copy_1">Print Style</div>
-                <div class="permissions" id="permissions_copy_1">Permissions</div>
-                <div data-type="subject" id="subject_copy_1">Subject</div>
-              </div>
-            </div>
-          HTML
-        )
+        ).to match_snapshot_auto
       end
     end
 
