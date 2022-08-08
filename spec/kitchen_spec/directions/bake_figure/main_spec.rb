783535dee8edb6adb790c360518d5d2e38beb005
ajluk <ajlukasik@gmail.com>
2022-08-08 17:35:27 +0000

    remove old bake_figure fix typo

--- spec/kitchen_spec/directions/bake_figure/main_spec.rb
+++ spec/kitchen_spec/directions/bake_figure/main_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe Kitchen::Directions::BakeFigure do
   it 'calls v1' do
     expect_any_instance_of(Kitchen::Directions::BakeFigure::V1).to receive(:bake)
       .with(figure: 'figure1', number: '7', cases: false)
-    described_class.v1(figure: 'figure1',number: '7', cases: false)
+    described_class.v1(figure: 'figure1', number: '7', cases: false)
   end
 end
