9e71d262f1d7455dffae3eeff8d4de797687b8b3
wilkus27 <natalia.wilkus@openstax.pl>
2022-08-16 13:26:22 +0000

    remove section title from answer key

--- lib/recipes/marketing/appendix_strategy.rb
+++ lib/recipes/marketing/appendix_strategy.rb
@@ -4,6 +4,7 @@
 class AppendixStrategy
   def bake(page:, append_to:)
     Kitchen::Directions::MoveSolutionsFromExerciseSection.v1(
-      chapter: page, append_to: append_to, section_class: 'knowledge-check', in_appendix: true)
+      chapter: page, append_to: append_to,
+      section_class: 'knowledge-check', options: { in_appendix: true })
   end
 end
