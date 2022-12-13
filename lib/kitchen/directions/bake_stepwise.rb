d27bc5b5e05e3b420092379947612e1b0b125750
Philip Schatz <253202+philschatz@users.noreply.github.com>
2022-12-13 18:51:32 +0000

    :bug: wrap stepwise content with a span/div

--- lib/kitchen/directions/bake_stepwise.rb
+++ lib/kitchen/directions/bake_stepwise.rb
@@ -4,12 +4,54 @@ module Kitchen
   module Directions
     module BakeStepwise
       def self.v1(book:)
+        # Block-level elements: https://developer.mozilla.org/en-US/docs/Web/HTML/Block-level_elements
+        block_level = %w[
+          address
+          article
+          aside
+          blockquote
+          details
+          dialog
+          dd
+          div
+          dl
+          dt
+          fieldset
+          figcaption
+          figure
+          footer
+          form
+          h1
+          h2
+          h3
+          h4
+          h5
+          h6
+          header
+          hgroup
+          hr
+          li
+          main
+          nav
+          ol
+          p
+          pre
+          section
+          table
+          ul
+        ]
         book.search('ol.stepwise').each do |ol|
           ol.remove_class('stepwise')
           ol.add_class('os-stepwise')
 
           ol.search('> li').each_with_index do |li, ii|
-            li.wrap_children('span', class: 'os-stepwise-content')
+            # If the list item contains block-level elements then wrap in a div
+            tag = if li.search(block_level.join(',')).to_a.length.positive?
+                    'div'
+                  else
+                    'span'
+                  end
+            li.wrap_children(tag, class: 'os-stepwise-content')
             li.prepend(child:
               <<~HTML
                 <span class="os-stepwise-token">#{I18n.t(:stepwise_step_label)} #{ii + 1}. </span>
