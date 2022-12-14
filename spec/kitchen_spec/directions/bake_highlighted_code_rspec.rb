34150831be337e17dc0f2683efa567293ec347fd
Mary Jo <mj81000@gmail.com>
2022-12-14 15:55:38 -0600

    adds data-lang to tagged code

---
+++ spec/kitchen_spec/directions/bake_highlighted_code_rspec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Kitchen::Directions::BakeInlineLists do
+  let(:book) do
+    book_containing(html:
+      <<~HTML
+        <div>
+          <code>This is code without a data-lang</code>
+          <code class="python">This code should have data-lang="python"</code>
+          <pre>This should not change</pre>
+          <pre class="python" data-type="python">This should not change</pre>
+        </div>
+      HTML
+    )
+  end
+
+  it 'works' do
+    described_class.v1(book: book)
+    expect(book.body).to match_snapshot_auto
+  end
+end
