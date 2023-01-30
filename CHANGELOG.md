55d06ab70446f1f3a058f9c76d3e097a490e991b
wilkus27 <natalia.wilkus@openstax.pl>
2023-01-30 14:22:04 +0000

    add section title to composite page header

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+* Create `UseSectionTitle` to move section title to composite page header
 * Remove `ElementBase#copied_id` since we don't copy IDs anymore
 
 ## [v1.22.0] - 2023-01-13
---
+++ lib/kitchen/directions/bake_eoc_section_content/use_section_title.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Kitchen
+  module Directions
+    module UseSectionTitle
+      def self.v1(chapter:, eoc_selector: '', section_selector: '')
+        composite_page = chapter.search(eoc_selector).first
+        section = composite_page.search(section_selector).first
+
+        section_title = section.first('[data-type="title"]')
+
+        composite_page_title = composite_page.first('[data-type="document-title"]')
+
+        composite_page_title.append(child:
+          <<~HTML
+            <span class="os-divider"> - </span>
+            <span class="os-subtitle">#{section_title.text}</span>
+          HTML
+        )
+
+        section_title.trash
+      end
+    end
+  end
+end
