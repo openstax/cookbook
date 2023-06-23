40dbdaef7707c8135923d2c7be500a312c6ef1b7
Mary Jo <mj81000@gmail.com>
2023-06-23 13:42:14 -0500

    Capitalize Oven Spec & Gemfile

--- spec/kitchen_spec/oven_spec.rb
+++ spec/kitchen_spec/oven_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Kitchen::Oven do
       end
 
       expect_bakes(input_xml: '<div></div>', recipes: recipe, output_xml: "<div>Howdy!</div>\n")
-      expect(I18n.t(:some_recipe_specific_term)).to match(/translation missing/)
+      expect(I18n.t(:some_recipe_specific_term)).to match(/Translation missing/)
     end
 
     it 'bakes with temporary, auto-discovered, recipe-specific locales' do
@@ -43,7 +43,7 @@ RSpec.describe Kitchen::Oven do
       end
 
       expect_bakes(input_xml: '<div></div>', recipes: recipe, output_xml: "<div>Auto-discovered!</div>\n")
-      expect(I18n.t(:some_recipe_specific_term)).to match(/translation missing/)
+      expect(I18n.t(:some_recipe_specific_term)).to match(/Translation missing/)
     end
 
     it 'autodetects the language' do
