54c2ee2ac6c4ae7343f0bf02bfa8bf7182f39c16
Mary Jo <mj81000@gmail.com>
2023-10-12 11:55:54 -0500

    rubocop fixes 2 electric boogaloo

--- lib/kitchen/directions/bake_numbered_table/bake_table_body.rb
+++ lib/kitchen/directions/bake_numbered_table/bake_table_body.rb
@@ -41,11 +41,11 @@ module Kitchen
           table.wrap(%(<div class="os-table">))
 
           table.search('th').each do |header|
-            header[:scope] =  if header[:colspan].nil? || header[:colspan] == '1'
-                                'col'
-                              else
-                                'colgroup'
-                              end
+            header[:scope] = if header[:colspan].nil? || header[:colspan] == '1'
+                               'col'
+                             else
+                               'colgroup'
+                             end
           end
 
           # Store label information
