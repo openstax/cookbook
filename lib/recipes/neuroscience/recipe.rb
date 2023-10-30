b220bebb8a765185347758a6d6111186539da602
Jennifer Wilson <26155528+jbwilson8@users.noreply.github.com>
2023-10-30 11:57:40 -0500

    add General book components

--- lib/recipes/neuroscience/recipe.rb
+++ lib/recipes/neuroscience/recipe.rb
@@ -8,12 +8,32 @@ NEUROSCIENCE_RECIPE = Kitchen::BookRecipe.new(book_short_name: :neuroscience) do
 
   book.search('cnx-pi').trash
 
+  BakeUnnumberedFigure.v1(book: book)
   BakePreface.v1(book: book)
-  #BakeImages.v1(book: book, resources: resources)
-
+  BakeUnitTitle.v1(book: book)
   BakeChapterTitle.v1(book: book)
 
-  BakeIndex.v1(book: book)
-  BakeToc.v1(book: book)
+  book.chapters.each do |chapter|
+    BakeLearningObjectives.v1(chapter: chapter)
+    BakeNonIntroductionPages.v1(chapter: chapter)
 
+    BakeChapterGlossary.v1(chapter: chapter, metadata_source: metadata)
+    BakeChapterSummary.v1(chapter: chapter, metadata_source: metadata, klass: 'summary')
+
+
+    chapter.figures(only: :figure_to_number?).each do |figure|
+      BakeFigure.v1(figure: figure,
+                    number: "#{chapter.count_in(:book)}.#{figure.count_in(:chapter)}")
+    end
+
+  end
+
+  BakeIndex.v1(book: book)
+  BakeFootnotes.v1(book: book)
+  BakeCompositePages.v1(book: book)
+  BakeCompositeChapters.v1(book: book)
+  BakeToc.v1(book: book)
+  BakeLinkPlaceholders.v1(book: book)
+  BakeFolio.v1(book: book)
+  BakeLinks.v1(book: book)
 end
