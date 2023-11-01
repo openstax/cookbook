865729a4559871f5976bfa7a05d3d66034ef39e0
Jennifer Wilson <jwilson.gsu2005@gmail.com>
2023-11-01 18:36:27 -0500

    Neuroscience recipe setup (#286)
    * initial recipe setup

---
+++ lib/recipes/neuroscience/recipe.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+NEUROSCIENCE_RECIPE = Kitchen::BookRecipe.new(book_short_name: :neuroscience) do |doc, _resources|
+  include Kitchen::Directions
+
+  book = doc.book
+  metadata = book.metadata
+
+  book.search('cnx-pi').trash
+
+  BakeUnnumberedFigure.v1(book: book)
+  BakePreface.v1(book: book)
+  BakeUnitTitle.v1(book: book)
+  BakeChapterTitle.v1(book: book)
+
+  book.chapters.each do |chapter|
+    BakeNonIntroductionPages.v1(chapter: chapter)
+    BakeChapterSummary.v1(chapter: chapter, metadata_source: metadata, klass: 'summary')
+
+    chapter.figures(only: :figure_to_number?).each do |figure|
+      BakeFigure.v1(figure: figure,
+                    number: "#{chapter.count_in(:book)}.#{figure.count_in(:chapter)}")
+    end
+
+    chapter.tables('$:not(.unnumbered)').each do |table|
+      BakeNumberedTable.v1(table: table,
+                           number: "#{chapter.count_in(:book)}.#{table.count_in(:chapter)}")
+    end
+
+  end
+
+  BakeIframes.v1(book: book)
+  BakeFootnotes.v1(book: book)
+  BakeCompositePages.v1(book: book)
+  BakeCompositeChapters.v1(book: book)
+  BakeToc.v1(book: book)
+  BakeLinkPlaceholders.v1(book: book)
+  BakeFolio.v1(book: book)
+  BakeLinks.v1(book: book)
+end
