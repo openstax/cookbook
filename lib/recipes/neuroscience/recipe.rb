306a1db4fd1054f5bac112837a049eb9c1498d73
Chris Kline <58607806+ckline-tryptic@users.noreply.github.com>
2023-11-03 21:54:31 +0000

    Added index and appendix to neuroscience

--- lib/recipes/neuroscience/recipe.rb
+++ lib/recipes/neuroscience/recipe.rb
@@ -65,6 +65,20 @@ NEUROSCIENCE_RECIPE = Kitchen::BookRecipe.new(book_short_name: :neuroscience) do
     end
   end
 
+  # Appendix
+  book.pages('$.appendix').each do |page|
+    appendix_letter = [*('A'..'Z')][page.count_in(:book) - 1]
+
+    page.figures(only: :figure_to_number?).each do |figure|
+      BakeFigure.v1(figure: figure, number: "#{appendix_letter}#{figure.count_in(:page)}")
+    end
+
+    page.tables('$:not(.unnumbered)').each do |table|
+      BakeNumberedTable.v1(table: table, number: "#{appendix_letter}#{table.count_in(:page)}")
+    end
+    BakeAppendix.v1(page: page, number: appendix_letter)
+  end
+
   BakeChapterIntroductions.v1(book: book)
   BakeIframes.v1(book: book)
   BakeFootnotes.v1(book: book)
@@ -72,6 +86,7 @@ NEUROSCIENCE_RECIPE = Kitchen::BookRecipe.new(book_short_name: :neuroscience) do
   BakeCompositeChapters.v1(book: book)
   BakeToc.v1(book: book)
   BakeLinkPlaceholders.v1(book: book)
+  BakeIndex.v1(book: book)
   BakeFolio.v1(book: book)
   BakeRexWrappers.v1(book: book)
   BakeLinks.v1(book: book)
