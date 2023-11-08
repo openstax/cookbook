9181447548dc1967435caa226424d110264eaaae
Kendra Swanson <kendraswan@gmail.com>
2023-11-08 16:45:37 +0000

    tests and rubocop

--- lib/recipes/web-generic/recipe.rb
+++ lib/recipes/web-generic/recipe.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-WEB_RECIPE = Kitchen::BookRecipe.new(book_short_name: :web) do |doc, resources|
+WEB_RECIPE = Kitchen::BookRecipe.new(book_short_name: :web) do |doc, _resources|
   include Kitchen::Directions
 
-  book = doc.book
+  _book = doc.book
 
   # BakeImages.v1(book: book, resources: resources)
 end
