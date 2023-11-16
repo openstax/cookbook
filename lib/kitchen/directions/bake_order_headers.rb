fc3e677cb4d5cfe4614186928ec33a4d5a018e47
Kendra Swanson <kendraswan@gmail.com>
2023-11-16 17:51:22 +0000

    create BakeOrderHeaders direction and spec

---
+++ lib/kitchen/directions/bake_order_headers.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Kitchen
+  module Directions
+    # Flattens all headers to order linearly after supplied top value
+    module BakeOrderHeaders
+      def self.v1(within:, top_header_value: 2)
+        # grab & sort set of all headers
+        header_set = [].to_set
+        within.search('h1,h2,h3,h4,h5,h6').each { |header| header_set.add(header.name) }
+        header_set = header_set.sort
+        # map header_set to promoted headers, shifted by top_header_value
+        header_map = {}
+        header_set.each_with_index do |name, index|
+          header_map[name] = "h#{index + top_header_value}"
+        end
+
+        # using the map, promote headers in place
+        within.search('h1,h2,h3,h4,h5,h6').each do |header|
+          # byebug
+          header.name = header_map[header.name]
+        end
+      end
+    end
+  end
+end
