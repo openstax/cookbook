ad54ed60328404b15349437ae7d1e68ce0ee20a6
Kendra Swanson <kendraswan@gmail.com>
2023-11-21 15:38:08 +0000

    update spec data

--- lib/kitchen/directions/bake_order_headers.rb
+++ lib/kitchen/directions/bake_order_headers.rb
@@ -2,7 +2,7 @@
 
 module Kitchen
   module Directions
-    # Flattens all headers to order linearly after supplied top value
+    # Bumps all headers to order after provided top header value
     module BakeOrderHeaders
       def self.v1(within:, top_header_value: 2)
         # grab & sort set of all headers
@@ -14,10 +14,8 @@ module Kitchen
         header_set.each_with_index do |name, index|
           header_map[name] = "h#{index + top_header_value}"
         end
-
         # using the map, promote headers in place
         within.search('h1,h2,h3,h4,h5,h6').each do |header|
-          # byebug
           header.name = header_map[header.name]
         end
       end
