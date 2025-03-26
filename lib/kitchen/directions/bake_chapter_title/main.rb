76d1520c6d9b227fa22b999668d7c969eee52370
Tyler Nullmeier <tylerzeromaster@gmail.com>
2025-03-26 15:48:29 -0500

    Add v2 bake_chapter_title that takes chapters as input

--- lib/kitchen/directions/bake_chapter_title/main.rb
+++ lib/kitchen/directions/bake_chapter_title/main.rb
@@ -6,6 +6,10 @@ module Kitchen
       def self.v1(book:, cases: false)
         V1.new.bake(book: book, cases: cases)
       end
+
+      def self.v2(chapters:, cases: false, numbering_options: {})
+        V2.new.bake(chapters: chapters, cases: cases, numbering_options: numbering_options)
+      end
     end
   end
 end
