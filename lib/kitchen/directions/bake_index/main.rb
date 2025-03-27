3dddbf118080659d3146cd75413e213235d906c1
Tyler Nullmeier <tylerzeromaster@gmail.com>
2025-03-27 13:23:12 -0500

    Unit numbering in bake_index

--- lib/kitchen/directions/bake_index/main.rb
+++ lib/kitchen/directions/bake_index/main.rb
@@ -3,8 +3,17 @@
 module Kitchen
   module Directions
     module BakeIndex
-      def self.v1(book:, types: %w[main], uuid_prefix: nil)
-        V1.new.bake(book: book, types: types, uuid_prefix: uuid_prefix)
+      def self.v1(book:,
+                  chapters: nil,
+                  types: %w[main],
+                  uuid_prefix: nil,
+                  numbering_options: { mode: :chapter_page, separator: '.' })
+        V1.new.bake(
+          book: book,
+          chapters: chapters || book.chapters,
+          types: types,
+          uuid_prefix: uuid_prefix,
+          numbering_options: numbering_options)
       end
     end
   end
