1295296d8cd30641d5d219a18cabc29a82dc9515
TylerZeroMaster <33585550+TylerZeroMaster@users.noreply.github.com>
2025-08-11 15:59:25 -0500

    CORE-1134 - Algebra 1 double numbering fix (#415)
    * Only bake numbered-exercises once
    
    - Add ids to a set
    - Bake numbered exercises sections in reverse order (deepest first)
    
    * Changelog
    
    * :shirt: Lint
    
    * Skip unnumbered exercises; use `baked?` instead of id set

--- lib/recipes/algebra-1/recipe.rb
+++ lib/recipes/algebra-1/recipe.rb
@@ -123,12 +123,16 @@ ALGEBRA_1_RECIPE = Kitchen::BookRecipe.new(book_short_name: :algebra1) do |doc, 
           end
         end
       end
-      chapter.pages.search('section.numbered-exercises').each do |section|
-        section.exercises.each_with_index do |exercise, idx|
-          options = { solution_stays_put: true, suppress_solution_title: true }
-          BakeNumberedExercise.v1(exercise: exercise,
-                                  number: idx + 1,
-                                  options: options)
+      chapter.pages.each do |page|
+        page.search('section.numbered-exercises').to_a.reverse.each do |section|
+          section.exercises('$:not(.unnumbered)').each_with_index do |exercise, idx|
+            next if exercise.baked?
+
+            options = { solution_stays_put: true, suppress_solution_title: true }
+            BakeNumberedExercise.v1(exercise: exercise,
+                                    number: idx + 1,
+                                    options: options)
+          end
         end
       end
       # Title added here
