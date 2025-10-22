e02667f754278d41f00f03fbb6804f7f7551df79
Tyler Nullmeier <tylerzeromaster@gmail.com>
2025-10-22 17:18:55 -0500

    Fix coverage
    The v1 of bake_order_headers was not completely covered because of how it works.

--- spec/kitchen_spec/directions/bake_order_headers_spec.rb
+++ spec/kitchen_spec/directions/bake_order_headers_spec.rb
@@ -17,6 +17,25 @@ RSpec.describe Kitchen::Directions::BakeOrderHeaders do
           <div><h6>subtitle subtitle</h6><p>paragraph</p>
             <h6>here we see a subtitle</h6>
           </div>
+        HTML
+      )
+    ).pages.first
+  end
+
+  let(:page_with_random_headers) do
+    book_containing(html:
+      one_chapter_with_one_page_containing(
+        <<~HTML
+          <h3>The top title within the page</h3>
+          <div>Body of introduction
+            <h6>Subtitle</h6>
+            <p>a paragraph</p>
+          </div>
+          <h3 data-type="document-title">wow another header</h3>
+          <h5>subheader also</h5>
+          <div><h6>subtitle subtitle</h6><p>paragraph</p>
+            <h6>here we see a subtitle</h6>
+          </div>
           <h1>The SS Morro Castle was</h1>
           <h3>an inside job</h3>
           <h6>(probably)</h6>
@@ -34,7 +53,7 @@ RSpec.describe Kitchen::Directions::BakeOrderHeaders do
   end
 
   it 'works (v2)' do
-    described_class.v2(within: page_with_headers)
-    expect(page_with_headers).to match_snapshot_auto
+    described_class.v2(within: page_with_random_headers)
+    expect(page_with_random_headers).to match_snapshot_auto
   end
 end
