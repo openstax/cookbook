95d4e21cdf0f7d19fc3ba771a2d8452d76d337a0
TylerZeroMaster <33585550+TylerZeroMaster@users.noreply.github.com>
2026-01-26 09:58:05 -0600

    Add `convert_textual_links` to `post_bake` recipe (#426)
    * Add `convert_textual_links` to `post_bake` recipe
    
    * Update spec data

--- lib/recipes/post_bake/recipe.rb
+++ lib/recipes/post_bake/recipe.rb
@@ -1,10 +1,57 @@
 # frozen_string_literal: true
 
+# Pre-compile the regex once
+URL_PATTERN = /(https?:\/\/[^\s<>"]+[^\s<>".,;:!?)\]}]|www\.[^\s<>"]+[^\s<>".,;:!?)\]}])/i
+QUICK_CHECK = /\.|:\/\/|www/ # Fast check before running heavy regex
+
+def convert_textual_links(element)
+  # 1. Prune the search: If we are in an <a> tag, stop recursing.
+  return if element.name == 'a'
+
+  # We use .children.to_a because we will be modifying the collection
+  # while iterating, and we don't want to break the iterator.
+  element.children.to_a.each do |child|
+    if child.text?
+      # 2. Quick Check: Don't run the complex regex if there's no '.' or '://'
+      next unless child.content =~ QUICK_CHECK
+
+      linkify_text_node(child)
+    elsif child.element?
+      convert_textual_links(child)
+    end
+  end
+end
+
+def linkify_text_node(node)
+  content = node.content
+  matches = content.scan(URL_PATTERN)
+  return if matches.empty?
+
+  fragment = node.document.create_element('span')
+  parts = content.split(URL_PATTERN)
+
+  parts.each do |part|
+    if part =~ URL_PATTERN
+      href = part.downcase.start_with?('www.') ? "https://#{part}" : part
+      # Create <a> tag directly
+      anchor = node.document.create_element('a', part, href: href)
+      fragment.add_child(anchor)
+    else
+      # Only add text nodes if they aren't empty
+      fragment.add_child(Nokogiri::XML::Text.new(part, node.document)) unless part.empty?
+    end
+  end
+
+  # Replace the single text node with all the new nodes at once
+  node.replace(fragment.children)
+end
+
 POST_BAKE = Kitchen::BookRecipe.new(book_short_name: :post_bake) do |doc|
   book = doc.book
 
   BakeOrderHeaders.v2(within: book)
 
+  convert_textual_links(doc.raw)
   book.search('a:not([aria-label])').each do |anchor|
     text_builder = anchor.search('.//text()').map { |t| t.text.strip }
     text_builder = text_builder.filter { |t| !t.empty? }
