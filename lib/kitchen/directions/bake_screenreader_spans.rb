81f39e235f0bd00ba8d7e55674f8421ca3cfb16c
Tyler Nullmeier <tylerzeromaster@gmail.com>
2026-03-24 10:14:04 -0500

    Add data-screenreader-begin/end to screenreader spans for PDF style accessibility

--- lib/kitchen/directions/bake_screenreader_spans.rb
+++ lib/kitchen/directions/bake_screenreader_spans.rb
@@ -7,38 +7,56 @@ module Kitchen
       # Additional screenreader spans can be added below.
       def self.v1(book:)
         book.search('u[data-effect="underline"]').each do |element|
+          begin_message = I18n.t(:'screenreader.underline')
+          end_message = "#{I18n.t(:'screenreader.end')} #{I18n.t(:'screenreader.underline')}"
           add_screenreader_text(
             element: element,
-            begin_message: I18n.t(:'screenreader.underline'),
-            end_message: "#{I18n.t(:'screenreader.end')} #{I18n.t(:'screenreader.underline')}"
+            begin_message: begin_message,
+            end_message: end_message
           )
         end
         book.search('u[data-effect="double-underline"]').each do |element|
+          begin_message = I18n.t(:'screenreader.double-underline')
+          end_message = "#{I18n.t(:'screenreader.end')} #{I18n.t(:'screenreader.double-underline')}"
+          element[:'data-screenreader-begin'] = begin_message
+          element[:'data-screenreader-end'] = end_message
           add_screenreader_text(
             element: element,
-            begin_message: I18n.t(:'screenreader.double-underline'),
-            end_message: "#{I18n.t(:'screenreader.end')} #{I18n.t(:'screenreader.double-underline')}"
+            begin_message: begin_message,
+            end_message: end_message
           )
         end
         book.search('p.public-domain').each do |element|
+          begin_message = I18n.t(:'screenreader.public-domain')
+          end_message = "#{I18n.t(:'screenreader.end')} #{I18n.t(:'screenreader.public-domain')}"
+          element[:'data-screenreader-begin'] = begin_message
+          element[:'data-screenreader-end'] = end_message
           add_screenreader_text(
             element: element,
-            begin_message: I18n.t(:'screenreader.public-domain'),
-            end_message: "#{I18n.t(:'screenreader.end')} #{I18n.t(:'screenreader.public-domain')}"
+            begin_message: begin_message,
+            end_message: end_message
           )
         end
         book.search('p.student-sample').each do |element|
+          begin_message = I18n.t(:'screenreader.student-sample')
+          end_message = "#{I18n.t(:'screenreader.end')} #{I18n.t(:'screenreader.student-sample')}"
+          element[:'data-screenreader-begin'] = begin_message
+          element[:'data-screenreader-end'] = end_message
           add_screenreader_text(
             element: element,
-            begin_message: I18n.t(:'screenreader.student-sample'),
-            end_message: "#{I18n.t(:'screenreader.end')} #{I18n.t(:'screenreader.student-sample')}"
+            begin_message: begin_message,
+            end_message: end_message
           )
         end
         book.search('p.annotation-text').each do |element|
+          begin_message = I18n.t(:'screenreader.annotation-text')
+          end_message = "#{I18n.t(:'screenreader.end')} #{I18n.t(:'screenreader.annotation-text')}"
+          element[:'data-screenreader-begin'] = begin_message
+          element[:'data-screenreader-end'] = end_message
           add_screenreader_text(
             element: element,
-            begin_message: I18n.t(:'screenreader.annotation-text'),
-            end_message: "#{I18n.t(:'screenreader.end')} #{I18n.t(:'screenreader.annotation-text')}"
+            begin_message: begin_message,
+            end_message: end_message
           )
         end
       end
