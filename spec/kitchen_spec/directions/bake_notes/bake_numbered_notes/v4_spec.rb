83b154925b0b5b287d52265b0cf112609a9b0fbb
Tyler Nullmeier <tylerzeromaster@gmail.com>
2026-07-08 15:48:40 -0500

    Coverage

--- spec/kitchen_spec/directions/bake_notes/bake_numbered_notes/v4_spec.rb
+++ spec/kitchen_spec/directions/bake_notes/bake_numbered_notes/v4_spec.rb
@@ -27,6 +27,27 @@ RSpec.describe Kitchen::Directions::BakeNumberedNotes::V4 do
               </div>
             </section>
           </div>
+          <div data-type="page" id="page_4">
+            <section class="wrapper">
+              <div data-type="note" id="6" class="foo">
+                <p>wrapped content with exercise</p>
+                <div data-type="exercise">
+                  <div data-type="problem">what is your quest?</div>
+                </div>
+              </div>
+              <div data-type="note" id="7" class="foo">
+                <p>wrapped content with injected question</p>
+                <div data-type="injected-exercise">
+                  <div data-type="exercise-question" data-id="26">
+                    <div data-type="question-stem">a question stem</div>
+                    <div data-type="question-solution">
+                      some solution
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>
         <div data-type="chapter">
           <div data-type="page" id="page_3">
@@ -57,7 +78,7 @@ RSpec.describe Kitchen::Directions::BakeNumberedNotes::V4 do
   it 'returns the ids of the notes it baked, so callers can exclude them elsewhere' do
     result = described_class.new.bake(book: book_with_notes, classes: %w[foo], within: 'section.wrapper',
                                       scope: :chapter)
-    expect(result).to contain_exactly('1', '2', '4', '5')
+    expect(result).to contain_exactly('1', '2', '4', '5', '6', '7')
   end
 
   context 'when a matched note has no id' do
